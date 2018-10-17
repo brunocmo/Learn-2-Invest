736353b56cfcf8105ad47811a74083620f52ba18
raimi <brunocmo.nunes@gmail.com>
2018-10-16 21:04:01 -0300

    new

--- telamain/src/androidTest/java/com/a3s/learn2invest/ExampleInstrumentedTest.java
+++ telamain/src/androidTest/java/com/a3s/learn2invest/ExampleInstrumentedTest.java
@@ -1,13 +1,13 @@
 package com.a3s.learn2invest;
 
-        import android.content.Context;
-        import android.support.test.InstrumentationRegistry;
-        import android.support.test.runner.AndroidJUnit4;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
 
-        import org.junit.Test;
-        import org.junit.runner.RunWith;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
-        import static org.junit.Assert.*;
+import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
