8c9eb2ae62a5ef4d772d32035e9701fe1c85cc12
lucaslop <lucaslopes05597@gmail.com>
2018-11-04 01:38:14 -0200

    Add function to buttons
    ajuda de @laisramos123
    
    Co-Authored-By: laisramos123 <laisramos123@users.noreply.github.com>

--- telamain/src/main/java/com/a3s/learn2invest/MainActivity.java
+++ telamain/src/main/java/com/a3s/learn2invest/MainActivity.java
@@ -12,6 +12,9 @@ import android.widget.Spinner;
 public class MainActivity extends AppCompatActivity {
     Spinner spinner;
     Button exemplos,simular;
+    String[] SPINNERVALUES = {};
+    Intent intent;
+    String SpinnerValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +22,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         exemplos= (Button) findViewById(R.id.ButtonExemplos);
         simular= (Button) findViewById(R.id.ButtonSimular);
+
 
         spinner = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter adapter = ArrayAdapter.createFromResource(this,R.array.opcoes,android.R.layout.
@@ -31,9 +36,7 @@ public class MainActivity extends AppCompatActivity {
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                if(position==1){
-                    startActivity(new Intent(MainActivity.this,CDB.class));
-                }
+                SpinnerValue = (String)spinner.getSelectedItem();
 
             }
 
@@ -42,7 +45,116 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        exemplos.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                switch(SpinnerValue){
+                    case "LC":
+                        intent = new Intent(MainActivity.this, LcExemplos.class);
+                        startActivity(intent);
+                        break;
+
+                    case "LCA":
+                        intent = new Intent(MainActivity.this, LcaExemplos.class);
+                        startActivity(intent);
+                        break;
+
+                    case "LCI":
+                        intent = new Intent(MainActivity.this, lciExemplos.class);
+                        startActivity(intent);
+                        break;
+
+                    case "CDB":
+                        intent = new Intent(MainActivity.this, CDB.class);
+                        startActivity(intent);
+                        break;
+
+                    case "Dêbentures":
+                        intent = new Intent(MainActivity.this, debenturesExemplo.class);
+                        startActivity(intent);
+                        break;
+
+                    case "Tesouro Direto":
+                        intent = new Intent(MainActivity.this, TesouroDiretoExemplos.class);
+                        startActivity(intent);
+                        break;
+
+                }
+            }
+        });
+
+
+        simular.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                switch(SpinnerValue){
+                    case "LC":
+                        intent = new Intent(MainActivity.this,Lcsimulador.class);
+                        startActivity(intent);
+                        break;
+
+                    case "LCA":
+                        intent = new Intent(MainActivity.this, LcaSimulador.class);
+                        startActivity(intent);
+                        break;
+
+                    case "LCI":
+                        intent = new Intent(MainActivity.this, LciSimulador.class);
+                        startActivity(intent);
+                        break;
+
+                    case "CDB":
+                        intent = new Intent(MainActivity.this, CDBsimulador.class);
+                        startActivity(intent);
+                        break;
+
+                    case "Dêbentures":
+                        intent = new Intent(MainActivity.this, debenturesSimulador.class);
+                        startActivity(intent);
+                        break;
+
+                    case "Tesouro Direto":
+                        intent = new Intent(MainActivity.this, TesouroDiretoSimulador.class);
+                        startActivity(intent);
+                        break;
+
+                }
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
     }
 
     
-}
+
---
+++ telamain/src/main/java/com/a3s/learn2invest/debenturesExemplo.java
@@ -0,0 +1,13 @@
+package com.a3s.learn2invest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class debenturesExemplo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_debentures_exemplo);
+    }
+}
