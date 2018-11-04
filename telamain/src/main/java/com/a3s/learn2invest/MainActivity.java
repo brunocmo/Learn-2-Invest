08c4c92d3a07612b93fe3cd8ea48f40a65505404
lucaslop <lucaslopes05597@gmail.com>
2018-11-03 22:45:59 -0300

    initial screen

--- telamain/src/main/java/com/a3s/learn2invest/MainActivity.java
+++ telamain/src/main/java/com/a3s/learn2invest/MainActivity.java
@@ -1,14 +1,47 @@
 package com.a3s.learn2invest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    Spinner spinner;
+    Button exemplos,simular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        exemplos= (Button) findViewById(R.id.ButtonExemplos);
+        simular= (Button) findViewById(R.id.ButtonSimular);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,R.array.opcoes,android.R.layout.
+                simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position==1){
+                    startActivity(new Intent(MainActivity.this,CDB.class));
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     
