b15be0033f621886f36cb3be567c2384bf333053
lucaslop <lucaslopes05597@gmail.com>
2018-11-04 04:36:06 -0200

    Add cards(CDB)

--- telamain/src/main/java/com/a3s/learn2invest/CDB.java
+++ telamain/src/main/java/com/a3s/learn2invest/CDB.java
@@ -2,12 +2,30 @@ package com.a3s.learn2invest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.a3s.learn2invest.Adapter.PostagemAdapter;
 
 public class CDB extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cdb);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclevVew);
+        //definindo layout
+        RecyclerView.LayoutManager layoutManager= new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        //definir o adapter
+        PostagemAdapter adapter= new PostagemAdapter();
+
+        recyclerView.setAdapter(adapter);
+
+
     }
 }
