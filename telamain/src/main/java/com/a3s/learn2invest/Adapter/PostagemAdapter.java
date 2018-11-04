afd50d11b63b3156a109cb0d2305bcd3017fc1ed
Lucas Lopes <lucaslopes05597@gmail.com>
2018-11-04 18:27:29 -0200

    Add script to exemples
    Co-Authored-By: laisramos123 <laisramos123@users.noreply.github.com>

--- telamain/src/main/java/com/a3s/learn2invest/Adapter/PostagemAdapter.java
+++ telamain/src/main/java/com/a3s/learn2invest/Adapter/PostagemAdapter.java
@@ -5,16 +5,24 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.a3s.learn2invest.Postagem;
 import com.a3s.learn2invest.R;
+
+import java.util.List;
 
 public class PostagemAdapter extends RecyclerView.Adapter <PostagemAdapter.MyViewHolder>{
 
+    private List<Postagem>postagems;
+    public PostagemAdapter(List<Postagem> p) {
+        this.postagems =p;
 
-    @NonNull
+    }
+
     @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+    public MyViewHolder onCreateViewHolder( ViewGroup viewGroup, int i) {
         View itemlista = LayoutInflater.from(viewGroup.getContext())
                 .inflate(R.layout.cdb_items,viewGroup,false);
 
@@ -23,33 +31,41 @@ public class PostagemAdapter extends RecyclerView.Adapter <PostagemAdapter.MyVie
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
-        myViewHolder.nome.setText("banco A");
-        myViewHolder.valorMinimo.setText("1200");
-        myViewHolder.nome.setText("banco B");
-        myViewHolder.valorMinimo.setText("1200");
-        myViewHolder.nome.setText("banco C");
-        myViewHolder.valorMinimo.setText("1200");
-        myViewHolder.nome.setText("banco D");
-        myViewHolder.valorMinimo.setText("1200");
+        Postagem postagem = postagems.get(i);
+        myViewHolder.nome.setText(postagem.getNome());
+        myViewHolder.montante.setText(postagem.getMontante());
+        myViewHolder.valorMinimo.setText(postagem.getInvestimentoMinimo());
+        myViewHolder.risco.setText(postagem.getRisco());
+        myViewHolder.imagem.setImageResource(postagem.getImagem());
+        myViewHolder.taxa.setText(postagem.getTaxa());
 
 
     }
 
     @Override
     public int getItemCount() {
-        return 15;
+        return postagems.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
         private TextView nome;
         private TextView valorMinimo;
-        //private TextView taxa;
+        private TextView risco;
+        private ImageView imagem;
+        private TextView montante;
+        private TextView taxa;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
 
             nome = itemView.findViewById(R.id.nome);
-            valorMinimo = itemView.findViewById(R.id.valor);
+            valorMinimo = itemView.findViewById(R.id.investimentoMinimo);
+            risco = itemView.findViewById(R.id.risco);
+            imagem = itemView.findViewById(R.id.imagem);
+            taxa = itemView.findViewById(R.id.taxa);
+            montante = itemView.findViewById(R.id.montante);
+
+
 
         }
     }
---
+++ telamain/src/main/java/com/a3s/learn2invest/Postagem.java
@@ -0,0 +1,70 @@
+package com.a3s.learn2invest;
+
+public class Postagem {
+    private String nome;
+    private String investimentoMinimo;
+    private String montante;
+    private int imagem;
+    private String risco;
+    private String taxa;
+
+    public Postagem() {
+    }
+
+    public Postagem(String nome, String investimentoMinimo, String montante, int imagem, String risco, String taxa) {
+        this.nome = nome;
+        this.investimentoMinimo = investimentoMinimo;
+        this.montante = montante;
+        this.imagem = imagem;
+        this.risco = risco;
+        this.taxa = taxa;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getInvestimentoMinimo() {
+        return investimentoMinimo;
+    }
+
+    public void setInvestimentoMinimo(String investimentoMinimo) {
+        this.investimentoMinimo = investimentoMinimo;
+    }
+
+    public String getMontante() {
+        return montante;
+    }
+
+    public void setMontante(String montante) {
+        this.montante = montante;
+    }
+
+    public int getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(int imagem) {
+        this.imagem = imagem;
+    }
+
+    public String getRisco() {
+        return risco;
+    }
+
+    public void setRisco(String risco) {
+        this.risco = risco;
+    }
+
+    public String getTaxa() {
+        return taxa;
+    }
+
+    public void setTaxa(String taxa) {
+        this.taxa = taxa;
+    }
+}
