be04777bdfc2a1d8d349a855120211c8d615e494
Lucas Lopes <lucaslopes05597@gmail.com>
2018-11-05 17:28:15 -0200

    Implementado tabela @brunaalmeidasantos

--- telamain/src/main/java/com/a3s/learn2invest/lciExemplos.java
+++ telamain/src/main/java/com/a3s/learn2invest/lciExemplos.java
@@ -45,5 +45,38 @@ public class lciExemplos extends AppCompatActivity {
         p = new Postagem("Caixa Economica","R$: 100.00","100.000",R.drawable.vermelho,"Risco: Baixo","12% a.a");
         this.postagens.add(p);
 
+        p = new Postagem("Modal Mais (Pós-fixado)","Investimento: R$ 1.000","Montante: 1.012,90", R.drawable.verde,"Risco: Baixo","Taxa: 85% do CDI");
+        this.postagens.add(p);
+
+        p = new Postagem("Modal Mais (Pré-fixado)","Investimento: R$ 1.000","Montante: 1.011,67", R.drawable.verde,"Risco: Baixo","Taxa: 4,91 a.a.");
+        this.postagens.add(p);
+
+        p = new Postagem("BTG Pactual S/A (Pré-fixado)","Investimento: R$ 1.000","Montante: 1.029,71", R.drawable.verde,"Risco: Baixo","Taxa: 6,0% a.a.");
+        this.postagens.add(p);
+
+        p = new Postagem("ABRK Financeira","Investimento: R$ 10.000","Montante: 13.847,10", R.drawable.verde,"Risco: Baixo","Taxa: 11,64% a.a.");
+        this.postagens.add(p);
+
+        p = new Postagem("Barigui Financeira","Investimento: R$ 10.000","Montante: 14.346,94", R.drawable.verde,"Risco: Baixo","Taxa: 11,04% a.a.");
+        this.postagens.add(p);
+
+        p = new Postagem("Barigui Cia Hipotecária","Investimento: R$ 5.000,00","Montante: 5.996,77", R.drawable.amarelo,"Risco: Médio","Taxa: 19,9% a.a.");
+        this.postagens.add(p);
+        p = new Postagem("Avista Financeira","Investimento: R$ 10.000","Montante: 15.889,32", R.drawable.amarelo,"Risco: Médio","Taxa: 12,48% a.a.");
+        this.postagens.add(p);
+
+        p = new Postagem("Facta Financeira (Pós-fixado)","Investimento: R$ 10.000","Montante: 12.039,41", R.drawable.amarelo,"Risco: Médio","Taxa: 9,87% a.a.");
+        this.postagens.add(p);
+
+        p = new Postagem("Barigui Cia Hipotecária","Investimento: R$ 10.000","Montante: R$ 11.993,53", R.drawable.amarelo,"Risco: Médio","Taxa: 19,9% a.a.");
+        this.postagens.add(p);
+
+        p = new Postagem("BTG Pactual S/A (Pré-fixado)","Investimento: R$ 10.000","Montante: 10.680,26", R.drawable.vermelho,"Risco: Alto","Taxa: 6,8 a.a.");
+        this.postagens.add(p);
+
+        p = new Postagem("Banco Ribeirão Preto (Pré-fixado)","Investimento: R$ 10.000","Montante: 14.857,07", R.drawable.vermelho,"Risco: Alto","Taxa: 6,84% a.a.");
+        this.postagens.add(p);
+
+
     }
 }
