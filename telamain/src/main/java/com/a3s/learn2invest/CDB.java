093a9dcbd3971c33488b0045e35e5f0782f090b4
Lucas Lopes <lucaslopes05597@gmail.com>
2018-11-05 23:48:58 -0200

    Add table part 1.2 pericles

--- telamain/src/main/java/com/a3s/learn2invest/CDB.java
+++ telamain/src/main/java/com/a3s/learn2invest/CDB.java
@@ -38,6 +38,59 @@ public class CDB extends AppCompatActivity {
     public void prepararPostagens(){
         Postagem p = new Postagem();
 
+        p = new Postagem("Modal (Pré-fixado)","Investimento R$:1.000","Montate:1.466,40",R.drawable.verde,"Risco: Baixo","Taxa: 7.70% a.a","Prazo: 2160 Dias");
+        this.postagens.add(p);
+
+        p = new Postagem("Modal (Pré-fixado)","Investimento R$:1.000","Montate:1.954,56",R.drawable.verde,"Risco: Baixo","Taxa: 7.96% a.a","Prazo: 3600 Dias");
+        this.postagens.add(p);
+
+
+        p = new Postagem("Modal (Pré-fixado)","Investimento R$:1.000","Montate:1.327,89",R.drawable.verde,"Risco: Baixo","Taxa: 8.42% a.a","Prazo: 1440 Dias");
+        this.postagens.add(p);
+
+        p = new Postagem("Modal (Pré-fixado)","Investimento R$:1.000","Montate:1.301,25",R.drawable.verde,"Risco: Baixo","Taxa: 8.52% a.a","Prazo: 1360 Dias");
+        this.postagens.add(p);
+
+        p = new Postagem("Modal (Pré-fixado)","Investimento R$:1.000","Montate:1.226,63",R.drawable.verde,"Risco: Baixo","Taxa: 8.22% a.a","Prazo: 1096 Dias");
+        this.postagens.add(p);
+
+        p = new Postagem("Modal (Pré-fixado)","Investimento R$:1.000","Montate:1.165,64",R.drawable.verde,"Risco: Baixo","Taxa: 7.53% a.a","Prazo: 900 Dias");
+        this.postagens.add(p);
+
+        p = new Postagem("Modal (Pré-fixado)","Investimento R$:1.000","Montate:1.052,05",R.drawable.amarelo,"Risco: Médio","Taxa: 6.31% a.a","Prazo: 181 Dias");
+        this.postagens.add(p);
+
+        p = new Postagem("Modal (Pré-fixado)","Investimento R$:1.000","Montate:1.052,05",R.drawable.amarelo,"Risco: Médio","Taxa: 6.31% a.a","Prazo: 181 Dias");
+        this.postagens.add(p);
+
+
+        p = new Postagem("Modal (Pré-fixado)","Investimento R$:1.000","Montate:1.127,08",R.drawable.amarelo,"Risco: Médio","Taxa: 7.23% a.a","Prazo: 731 Dias");
+        this.postagens.add(p);
+
+        p = new Postagem("Modal (Pré-fixado)","Investimento R$:1.000","Montate:1.139,88",R.drawable.amarelo,"Risco: Médio","Taxa: 7.23% a.a","Prazo: 800 Dias");
+        this.postagens.add(p);
+
+        p = new Postagem("Modal (Pós-fixado)","Investimento R$:1.000","Montate:1.003,26",R.drawable.vermelho,"Risco: Alto","Taxa: 85% do CDI","Prazo: 30 Dias");
+        this.postagens.add(p);
+
+        p = new Postagem("Modal (Pré-fixado)","Investimento R$:1.000","Montate:1.003,11",R.drawable.vermelho,"Risco: Alto","Taxa: 5.18% a.a","Prazo: 30 Dias");
+        this.postagens.add(p);
+
+        p = new Postagem("Modal (Pré-fixado)","Investimento R$:1.000","Montate:1.006,69",R.drawable.vermelho,"Risco: Alto","Taxa: 5.30% a.a","Prazo: 60 Dias");
+        this.postagens.add(p);
+
+        p = new Postagem("Modal (Pré-fixado)","Investimento R$:1.000","Montate:1.009,69",R.drawable.vermelho,"Risco: Alto","Taxa: 5.36% a.a","Prazo: 90 Dias");
+        this.postagens.add(p);
+
+        p = new Postagem("Modal (Pré-fixado)","Investimento R$:1.000","Montate:1.013,49",R.drawable.vermelho,"Risco: Alto","Taxa: 5.45% a.a","Prazo: 120 Dias");
+        this.postagens.add(p);
+
+        p = new Postagem("Modal (Pré-fixado)","Investimento R$:1.000","Montate:1.016,79",R.drawable.vermelho,"Risco: Alto","Taxa: 5.44% a.a","Prazo: 150 Dias");
+        this.postagens.add(p);
+
+        p = new Postagem("Modal (Pré-fixado)","Investimento R$:1.000","Montate:1.021,00",R.drawable.vermelho,"Risco: Alto","Taxa: 5.50% a.a","Prazo: 181 Dias");
+        this.postagens.add(p);
+
 
     }
 }
--- telamain/src/main/java/com/a3s/learn2invest/LcExemplos.java
+++ telamain/src/main/java/com/a3s/learn2invest/LcExemplos.java
@@ -37,59 +37,58 @@ public class LcExemplos extends AppCompatActivity {
     public void prepararPostagens(){
         Postagem p = new Postagem();
 
-        p = new Postagem("Senff (Pós-fixado)","Investimento R$:1.000","Montate: 1.365,48",R.drawable.verde,"Risco: Baixo","Taxa: 118% do CDI","Prazo: 1800 Dias");
+        p = new Postagem("Senff (Pós-fixado)","Investimento R$:1.000","Montate:1.365,48",R.drawable.verde,"Risco: Baixo","Taxa: 118% do CDI","Prazo: 1800 Dias");
         this.postagens.add(p);
 
-        p = new Postagem("Senff (Pós-fixado)","Investimento R$:1.000","Montate: 1.201,67",R.drawable.verde,"Risco: Baixo","Taxa: 117% do CDI","Prazo: 1080 Dias");
+        p = new Postagem("Senff (Pós-fixado)","Investimento R$:1.000","Montate:1.201,67",R.drawable.verde,"Risco: Baixo","Taxa: 117% do CDI","Prazo: 1080 Dias");
         this.postagens.add(p);
 
-        p = new Postagem("Senff (Pós-fixado)","Investimento R$:1.000","Montate: 1.279,12",R.drawable.verde,"Risco: Baixo","Taxa: 117% do CDI","Prazo: 1440 Dias");
+        p = new Postagem("Senff (Pós-fixado)","Investimento R$:1.000","Montate:1.279,12",R.drawable.verde,"Risco: Baixo","Taxa: 117% do CDI","Prazo: 1440 Dias");
         this.postagens.add(p);
 
-        p = new Postagem("Caruana (Pré-fixado)","Investimento R$:3.000","Montate: 3.406,07",R.drawable.verde,"Risco: Baixo","Taxa: 7.70% a.a","Prazo: 730 Dias");
+        p = new Postagem("Caruana (Pré-fixado)","Investimento R$:3.000","Montate:3.406,07",R.drawable.verde,"Risco: Baixo","Taxa: 7.70% a.a","Prazo: 730 Dias");
         this.postagens.add(p);
 
-        p = new Postagem("Caruana (Pré-fixado)","Investimento R$:3.000","Montate: 4.512,05",R.drawable.verde,"Risco: Baixo","Taxa: 9.80% a.a","Prazo: 1825 Dias");
+        p = new Postagem("Caruana (Pré-fixado)","Investimento R$:3.000","Montate:4.512,05",R.drawable.verde,"Risco: Baixo","Taxa: 9.80% a.a","Prazo: 1825 Dias");
         this.postagens.add(p);
 
-        p = new Postagem("Caruana (Pré-fixado)","Investimento R$:3.000","Montate: 3.406,07",R.drawable.verde,"Risco: Baixo","Taxa: 9.35% a.a","Prazo: 1460 Dias");
+        p = new Postagem("Caruana (Pré-fixado)","Investimento R$:3.000","Montate:3.406,07",R.drawable.verde,"Risco: Baixo","Taxa: 9.35% a.a","Prazo: 1460 Dias");
         this.postagens.add(p);
 
-        p = new Postagem("Caruana (Pré-fixado)","Investimento R$:3.000","Montate: 3.721,88",R.drawable.verde,"Risco: Baixo","Taxa: 8.70% a.a","Prazo: 1095 Dias");
+        p = new Postagem("Caruana (Pré-fixado)","Investimento R$:3.000","Montate:3.721,88",R.drawable.verde,"Risco: Baixo","Taxa: 8.70% a.a","Prazo: 1095 Dias");
         this.postagens.add(p);
 
-        p = new Postagem("Caruana (Pós-fixado)","Investimento R$:3.000","Montate: 4.468,92",R.drawable.verde,"Risco: Baixo","Taxa: 123,50% do CDI","Prazo: 2190 Dias");
+        p = new Postagem("Caruana (Pós-fixado)","Investimento R$:3.000","Montate:4.468,92",R.drawable.verde,"Risco: Baixo","Taxa: 123,50% do CDI","Prazo: 2190 Dias");
         this.postagens.add(p);
 
-        p = new Postagem("Caruana (Pós-fixado)","Investimento R$:3.000","Montate: 3891,56",R.drawable.verde,"Risco: Baixo","Taxa: 122,00% do CDI","Prazo: 1460 Dias");
+        p = new Postagem("Caruana (Pós-fixado)","Investimento R$:3.000","Montate:3891,56",R.drawable.verde,"Risco: Baixo","Taxa: 122,00% do CDI","Prazo: 1460 Dias");
         this.postagens.add(p);
 
-        p = new Postagem("Caruana (Pós-fixado)","Investimento R$:3.000","Montate: 4.163,54",R.drawable.verde,"Risco: Baixo","Taxa: 122,50% do CDI","Prazo: 1825 Dias");
+        p = new Postagem("Caruana (Pós-fixado)","Investimento R$:3.000","Montate:4.163,54",R.drawable.verde,"Risco: Baixo","Taxa: 122,50% do CDI","Prazo: 1825 Dias");
         this.postagens.add(p);
 
-        p = new Postagem("Caruana (Pós-fixado)","Investimento R$:3.000","Montate: 3.636,64",R.drawable.verde,"Risco: Baixo","Taxa: 121,00% do CDI","Prazo: 1095 Dias");
+        p = new Postagem("Caruana (Pós-fixado)","Investimento R$:3.000","Montate:3.636,64",R.drawable.verde,"Risco: Baixo","Taxa: 121,00% do CDI","Prazo: 1095 Dias");
         this.postagens.add(p);
 
-        p = new Postagem("Senff (Pós-fixado)","Investimento R$:1.000","Montate: 1.129,53",R.drawable.amarelo,"Risco: Médio","Taxa: 117% do CDI","Prazo: 721 Dias");
+        p = new Postagem("Senff (Pós-fixado)","Investimento R$:1.000","Montate:1.129,53",R.drawable.amarelo,"Risco: Médio","Taxa: 117% do CDI","Prazo: 721 Dias");
         this.postagens.add(p);
 
-        p = new Postagem("Senff (Pós-fixado)","Investimento R$:1.000","Montate: 1.090,12",R.drawable.amarelo,"Risco: Médio","Taxa: 114% do CDI","Prazo: 540 Dias");
+        p = new Postagem("Senff (Pós-fixado)","Investimento R$:1.000","Montate:1.090,12",R.drawable.amarelo,"Risco: Médio","Taxa: 114% do CDI","Prazo: 540 Dias");
         this.postagens.add(p);
 
-        p = new Postagem("Caruana (Pós-fixado)","Investimento R$:3.000","Montate: 3.401,48",R.drawable.amarelo,"Risco: Médio","Taxa: 119,00% do CDI","Prazo: 730 Dias");
+        p = new Postagem("Caruana (Pós-fixado)","Investimento R$:3.000","Montate:3.401,48",R.drawable.amarelo,"Risco: Médio","Taxa: 119,00% do CDI","Prazo: 730 Dias");
         this.postagens.add(p);
 
-        p = new Postagem("Senff (Pós-fixado)","Investimento R$:1.000","Montate: 1.059,66",R.drawable.vermelho,"Risco: Alto","Taxa: 113% do CDI","Prazo: 365 Dias");
+        p = new Postagem("Senff (Pós-fixado)","Investimento R$:1.000","Montate:1.059,66",R.drawable.vermelho,"Risco: Alto","Taxa: 113% do CDI","Prazo: 365 Dias");
         this.postagens.add(p);
 
-        p = new Postagem("Senff (Pós-fixado)","Investimento R$:1.000","Montate: 1.025,11",R.drawable.vermelho,"Risco: Alto","Taxa: 103% do CDI","Prazo: 181 Dias");
+        p = new Postagem("Senff (Pós-fixado)","Investimento R$:1.000","Montate:1.025,11",R.drawable.vermelho,"Risco: Alto","Taxa: 103% do CDI","Prazo: 181 Dias");
         this.postagens.add(p);
 
-
-        p = new Postagem("Caruana (Pós-fixado)","Investimento R$:3.000","Montate: 3.182,90",R.drawable.vermelho,"Risco: Alto","Taxa: 115,00% do CDI","Prazo: 365 Dias");
+        p = new Postagem("Caruana (Pós-fixado)","Investimento R$:3.000","Montate:3.182,90",R.drawable.vermelho,"Risco: Alto","Taxa: 115,00% do CDI","Prazo: 365 Dias");
         this.postagens.add(p);
 
-        p = new Postagem("Caruana (Pós-fixado)","Investimento R$:3.000","Montate: 3.078,92",R.drawable.vermelho,"Risco: Alto","Taxa: 108,00% do CDI","Prazo: 181 Dias");
+        p = new Postagem("Caruana (Pós-fixado)","Investimento R$:3.000","Montate:3.078,92",R.drawable.vermelho,"Risco: Alto","Taxa: 108,00% do CDI","Prazo: 181 Dias");
         this.postagens.add(p);
 
 
